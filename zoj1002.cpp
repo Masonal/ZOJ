71b5bd75240ef0a489c1ac26d95393deb9608d37
Masonal <15215513208@163.com>
2018-03-22 10:59:59 +0800

    zoj1002.cpp
    看了3个小时才看完一题。。。没事没事，心态不炸，问题不大

---
+++ zoj1002.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+char map[4][4];
+int maxNum, n;
+
+
+bool canPut(int R, int C) {
+	int i;
+	for (i = R - 1; i >= 0; --i) 
+	{
+		if (map[i][C] == 'O') return false;
+		if (map[i][C] == 'X') break;
+	}
+	for (i = C - 1; i >= 0; --i) 
+	{
+		if (map[R][i] == 'O') return false;
+		if (map[R][i] == 'X') break;
+	}
+	return true;
+}
+
+
+void Solve(int k, int curNum) {
+	int row, col;
+	if (k == n*n) 
+	{
+		if (curNum > maxNum) 
+		{
+			maxNum = curNum;
+			return;
+		}
+	}
+	else 
+	{
+		row = k / n;
+		col = k % n;
+		if ((map[row][col] == '.') && (canPut(row, col) == true)) 
+		{
+			map[row][col] = 'O';
+			Solve(k + 1, curNum + 1);
+			map[row][col] = '.';
+		}
+		Solve(k + 1, curNum);
+	}
+
+}
+
+int main()
+{
+	int i, j;
+	while (cin >> n&&n != 0)
+	{
+		for (i = 0; i<n; i++)
+		{
+			for (j = 0; j<n; j++)
+			{
+				cin >> map[i][j];
+			}
+		}
+		maxNum = 0;
+		Solve(0, 0);
+		cout << maxNum << endl;
+	}
+	return 0;
+}
